3e5b2e3576f1400fbe88620271834d6e0d5fab34
Ava Smith <ava013005@gmail.com>
2023-08-16 16:03:31 -0500

    finished

---
+++ src/test/java/CohortTest.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class CohortTest {
+    private Cohort emptyCohort;
+    private Cohort actualCohort;
+
+    @Before
+    public void setup() {
+        actualCohort = new Cohort();
+    }
+
+    @Test
+    public void testCohortConstructors() {
+        assertNull(emptyCohort);
+        assertNotNull(actualCohort);
+
+    }
+
+    @Test
+    public void testCohortAddStudent() {
+        Student actualStudent = new Student(1l, "Ava");
+        actualCohort.addStudent(actualStudent);
+        assertEquals(Arrays.asList(actualStudent), actualCohort.getStudents());
+    }
+    @Test
+    public void testCohortGetStudents() {
+
+    }
+
+    @Test
+    public void testCohortGetCohortAverage(){
+        Student actualStudent = new Student(1L, "Ava");
+        actualStudent.addGrade(50);
+        actualStudent.addGrade(75);
+        actualStudent.addGrade(100);
+
+        actualCohort.addStudent(actualStudent);
+
+        assertEquals(75, actualCohort.getCohortAverage(), 0.000001);
+    }
+}
